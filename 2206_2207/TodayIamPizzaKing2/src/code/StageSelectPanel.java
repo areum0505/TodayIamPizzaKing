682315c8dca6055159a8acad58815be39625f6ac
gimyoni <hie0908@naver.com>
2020-10-06 11:06:25 +0900

    StageSelectPanel 버튼 추가, stageSelectBackground 수정

--- 2206_2207/TodayIamPizzaKing2/src/code/StageSelectPanel.java
+++ 2206_2207/TodayIamPizzaKing2/src/code/StageSelectPanel.java
@@ -12,10 +12,82 @@ import javax.swing.JPanel;
 public class StageSelectPanel extends JPanel{
 	private Image backgroundImage = new ImageIcon("images/main/stageSelectBackground.png").getImage();
 	
+	public JButton sauceBtn = new JButton();
+	public JButton cheeseBtn = new JButton();
+	public JButton onionBtn = new JButton();
+	public JButton paprikaBtn = new JButton();
+	public JButton pepperoniBtn = new JButton();
+	public JButton mushroomBtn = new JButton();
+	public JButton BagBtn = new JButton();
+	
 	public StageSelectPanel(Game game) {
 		setLayout(null);
 		setBounds(0, 0, 1280, 720);
-	
+		
+		sauceBtn.setVisible(true);
+		sauceBtn.setBounds(165, 155, 275, 170);
+		sauceBtn.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
+		this.add(sauceBtn);
+		
+		cheeseBtn.setVisible(true);
+		cheeseBtn.setBounds(520, 45, 275, 160);
+		cheeseBtn.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
+		this.add(cheeseBtn);
+		
+		onionBtn.setVisible(true);
+		onionBtn.setBounds(875, 145, 270, 195);
+		onionBtn.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
+		this.add(onionBtn);
+		
+		paprikaBtn.setVisible(true);
+		paprikaBtn.setBounds(155, 415, 265, 165);
+		paprikaBtn.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
+		this.add(paprikaBtn);
+		
+		pepperoniBtn.setVisible(true);
+		pepperoniBtn.setBounds(520, 505, 270, 165);
+		pepperoniBtn.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
+		this.add(pepperoniBtn);
+		
+		mushroomBtn.setVisible(true);
+		mushroomBtn.setBounds(875, 430, 250, 155);
+		mushroomBtn.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
+		this.add(mushroomBtn);
+		
+		BagBtn.setVisible(true);
+		BagBtn.setBounds(520, 270, 275, 173);
+		BagBtn.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
+		this.add(BagBtn);
+		
+		
 	}
 	
 	@Override
